650fff4793ab58977af58088d2abecd07edec722
ryanskeesuck <ryanskeesuck@u.boisestate.edu>
2018-09-19 19:59:31 -0600

    first commit

---
+++ 01main.php
@@ -0,0 +1,10 @@
+<html>
+  <?php require_once "00header.php"; ?>
+  <?php require_once "00navbar.php"; ?>
+    <div id="content">
+        <?php require_once "table.php";
+        render_table("icecream.txt");
+      ?>
+    </div>
+  </body>
+</html>
