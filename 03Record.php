c620946528778483668cd99b7c5f19f65938a60d
ryanskeesuck <ryanskeesuck@u.boisestate.edu>
2018-11-05 14:58:28 -0700

    Monday #13

--- 03Record.php
+++ 03Record.php
@@ -19,25 +19,21 @@
         <input type="text" name="lastname">
       <br>
       <h3>Gender of Student (if known)</h3>
-      <br>
         <input type="radio" name="gender" value="male" checked> Male<br>
         <input type="radio" name="gender" value="female"> Female<br>
         <input type="radio" name="gender" value="other"> Other
       <br>
       <h3>Grade of Student (if known)</h3>
-      <br>
         <input type="radio" name="grade" value="6th Grade" checked> 6th Grade<br>
         <input type="radio" name="grade" value="7th Grade"> 7th Grade<br>
         <input type="radio" name="grade" value="8th Grade"> 8th Grade<br>
         <input type="radio" name="grade" value="unknown"> Unknown<br>
       <br>
       <h3>What day did this happen?</h3>
-      <br>
         Date of Incident:
         <input type="date" name="date of incident">
       <br>
       <h3>What time of day did this happen?</h3>
-      <br>
         <input type="radio" name="time of day" value="Before School" checked> Before School<br>
         <input type="radio" name="time of day" value="Morning Break"> Morning Break<br>
         <input type="radio" name="time of day" value="Lunch"> Lunch<br>
@@ -46,7 +42,6 @@
         <input type="radio" name="time of day" value="During Class"> During Class<br>
       <br>
       <h3>If this happened during class, which period?</h3>
-      <br>
         <input type="radio" name="period" value="N/A" checked> N/A<br>
         <input type="radio" name="period" value="Advisory"> Advisory<br>
         <input type="radio" name="period" value="1st Period"> 1st Period<br>
@@ -59,7 +54,6 @@
         <input type="radio" name="period" value="8th Period"> 8th Period<br>
       <br>
       <h3>Where did this happen?</h3>
-      <br>
         <input type="radio" name="where" value="Classroom" checked> Classroom<br>
         <input type="radio" name="where" value="Cafeteria"> Cafeteria<br>
         <input type="radio" name="where" value="Library (During Open Use)"> Library<br>
@@ -73,7 +67,6 @@
         <input type="radio" name="where" value="Assembly/Activity"> Assembly/Activity<br>
       <br>
       <h3>BEHAVIOR(s) that primarily affect only the student:</h3>
-      <br>
         <input type="checkbox" name="behavior1" value="Not Following Directions"> Not Following Directions<br>
         <input type="checkbox" name="behavior2" value="Not Prepared for Class/Materials"> Not Prepared for Class/Materials<br>
         <input type="checkbox" name="behavior3" value="Off Task"> Off Task<br>
@@ -85,7 +78,6 @@
         <input type="checkbox" name="behavior9" value="Insubordination"> Insubordination<br>
       <br>
       <h3>BEHAVIOR(s) that affect(s) the student AND others:</h3>
-      <br>
         <input type="checkbox" name="behavior10" value="Talking During Instruction"> Talking During Instruction<br>
         <input type="checkbox" name="behavior11" value="Blurting Out"> Blurting Out<br>
         <input type="checkbox" name="behavior12" value="Physical Touching"> Physical Touching<br>
@@ -102,7 +94,6 @@
         <input type="checkbox" name="behavior23" value="Bullying"> Bullying<br>
       <br>
       <h3>BEHAVIOR(s) that affect(s) public safety and the common space:</h3>
-      <br>
         <input type="checkbox" name="behavior24" value="Running"> Running<br>
         <input type="checkbox" name="behavior25" value="Playing in Unapproved Areas"> Playing in Unapproved Areas<br>
         <input type="checkbox" name="behavior26" value="Littering"> Littering<br>
@@ -110,12 +101,10 @@
         <input type="checkbox" name="behavior28" value="Bringing Banned Substance(s) or Proprty"> Banned Substance(s) or Property<br>
     <br>
     <h3>Any other behavior(s) or observations that we need to know about?</h3>
-    <br>
         <textarea name="message" rows="10" cols="50">Type your message here...</textarea>
     <br>
       <br>
-      <h3>ACTION(s) taken by the staff member to address teh behavior.</h3>
-      <br>
+      <h3>ACTION(s) taken by the staff member to address the behavior.</h3>
         <input type="checkbox" name="action1" value="No Action/Observation"> No Action/Observation<br>
         <input type="checkbox" name="action2" value="Gave Verbal Warning"> Gave Verbal Warning<br>
         <input type="checkbox" name="action3" value="Private Student Conference"> Private Student Conference<br>
@@ -126,13 +115,13 @@
         <input type="checkbox" name="action8" value="Sent to Office (ODR)"> Sent to Office (ODR)<br>
         <br>
         <h3>Submit Your Form!</h3>
-        <br>
         <input type="submit" value="Submit">
       </form>
         <br>
+        <div>
         <h3>Would You Like to Record a New Incident?</h3>
-        <br>
         <a href="03Record.php" class="button">Record New</a>
+      </div>
       </div>
 
     </div>
