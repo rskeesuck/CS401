cbefd1a07d08810e61c6e14af6e41cf8c04b97ca
ryanskeesuck <ryanskeesuck@u.boisestate.edu>
2018-11-05 16:32:24 -0700

    Monday #26

--- 07About.php
+++ 07About.php
@@ -20,17 +20,14 @@
           <img src="Coder.jpg" alt= "Hello World!" width="100%" class = "center">
       </div>
 
-      <div>
+      <div class = "address">
         <h4>This is where you can find us:</h4>
         <br>
-      </div>
-
-      <div class = "address">
         <p>School BROS, Inc.</p>
         <p>12345 N Main Street</p>
         <p>Anytown, USA</p>
         <p>o: 1.208.555.1234</p>
-        <p>e: info@schoolbros.com</p>
+        <p>e: fakeaddress@schoolbros.com</p>
         <br>
         </div>
 
