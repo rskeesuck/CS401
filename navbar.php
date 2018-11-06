bf82aaef9a2a294b17ad4807672fbab9138beba9
ryanskeesuck <ryanskeesuck@u.boisestate.edu>
2018-11-05 17:53:16 -0700

    Monday #31

--- navbar.php
+++ navbar.php
@@ -1,15 +1,17 @@
 <html>
-<div class="navbar">
-  <ul>
-      <li><a href="00MainPage.php">Home</a></li>
-      <li><a href="06Help.php">Help/FAQ</a></li>
-      <li><a href="07About.php">About Us</a></li>
-      <li><a href="02UserHome.php">My Page</a></li>
-  </ul>
-</div>
-<div class="navbar">
-    <ul>
-    <li><a href="01LogonPage.php">Logon</a></li>
-    </ul>
+<div>
+  <div class="navbar">
+      <ul>
+        <li><a href="00MainPage.php">Home</a></li>
+        <li><a href="06Help.php">Help/FAQ</a></li>
+        <li><a href="07About.php">About Us</a></li>
+        <li><a href="02UserHome.php">My Page</a></li>
+      </ul>
+  </div>
+  <div class="navbar">
+      <ul>
+        <li><a href="01LogonPage.php">Logon</a></li>
+      </ul>
+  </div>
 </div>
 </html>
