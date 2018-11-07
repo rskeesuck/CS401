5d412103ce9455dc15d178c004d7c7a87355e99a
ryanskeesuck <ryanskeesuck@u.boisestate.edu>
2018-11-06 21:03:58 -0700

    Tuesday #17

--- navbar.php
+++ navbar.php
@@ -2,13 +2,13 @@
   <div class="navbar">
     <li <?php if ($thisPage == '00MainPage') echo  " id=\"currentpage\""; ?>>
       <a href="00MainPage.php">Home</a></li>
-    <li> <?php if ($thisPage == '06Help') echo 'id="currentpage"'; ?>
+    <li> <?php if ($thisPage == '06Help') echo  " id=\"currentpage\""; ?>
       <a href="06Help.php">Help/FAQ</a></li>
-    <li> <?php if ($thisPage == '07About') echo ' id=\"currentpage\"'; ?>
+    <li> <?php if ($thisPage == '07About') echo  " id=\"currentpage\""; ?>
       <a href="07About.php">About Us</a></li>
-    <li> <?php if ($thisPage == '01LogonPage') echo ' id=\"currentpage\"'; ?>
+    <li> <?php if ($thisPage == '01LogonPage') echo  " id=\"currentpage\""; ?>
       <a href="01LogonPage.php">Logon</a></li>
-    <li> <?php if ($thisPage == "02UserHome") echo ' id=\"currentpage\"'; ?>
-      <a href="02UserHome.php">VOTE</a></li>
+    <li> <?php if ($thisPage == "02UserHome") echo  " id=\"currentpage\""; ?>
+      <a href="02UserHome.php">User Home</a></li>
   </div>
 </html>
