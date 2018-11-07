3a67e3c438c3c87f17c7dbd0d3630b4a1f9b688e
ryanskeesuck <ryanskeesuck@u.boisestate.edu>
2018-11-06 22:44:43 -0700

    Tuesday #27

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
-  header('Location: 01LogonPage.php');
-  exit;
-}
-
 include_once("00MainPage.php");
 
 ?>
