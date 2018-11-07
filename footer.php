18669a23b452a1f109364434216bf66a1d3ad6bf
ryanskeesuck <ryanskeesuck@u.boisestate.edu>
2018-11-06 21:49:21 -0700

    Tuesday #27

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
-<div id = "footer">
-  <ul>
-    <li><a href="00MainPage.php">Home</a></li>
-    <li><a href="08Secret.php">&copy Ryan Skeesuck</a></li>
-    <li><a href="07About.php">Contact</a></li>
-  </ul>
-</id>;
+<div/ id="footerdiv">
+  <div id = "footer">
+    <ul>
+      <li><a href="00MainPage.php">Home</a></li>
+      <li><a href="08Secret.php">&copy Ryan Skeesuck</a></li>
+      <li><a href="07About.php">Contact</a></li>
+    </ul>
+  </div>
+</div>
