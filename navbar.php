39a9614e23dc66505f710f5d7813416b1e5ff682
ryanskeesuck <ryanskeesuck@u.boisestate.edu>
2018-11-06 21:17:07 -0700

    Tuesday #18

--- navbar.php
+++ navbar.php
@@ -1,14 +1,21 @@
 <html>
-  <div class="navbar">
-    <li <?php if ($thisPage == '00MainPage') echo  " id=\"currentpage\""; ?>>
-      <a href="00MainPage.php">Home</a></li>
-    <li> <?php if ($thisPage == '06Help') echo  " id=\"currentpage\""; ?>
-      <a href="06Help.php">Help/FAQ</a></li>
-    <li> <?php if ($thisPage == '07About') echo  " id=\"currentpage\""; ?>
-      <a href="07About.php">About Us</a></li>
-    <li> <?php if ($thisPage == '01LogonPage') echo  " id=\"currentpage\""; ?>
-      <a href="01LogonPage.php">Logon</a></li>
-    <li> <?php if ($thisPage == "02UserHome") echo  " id=\"currentpage\""; ?>
-      <a href="02UserHome.php">User Home</a></li>
-  </div>
+  <div id="navigation">
+      <ul>
+        <li<?php if ($thisPage=="00MainPage")
+        echo " id=\"currentpage\""; ?>>
+        <a href="#">Home</a></li>
+        <li<?php if ($thisPage=="06Help")
+        echo " id=\"currentpage\""; ?>>
+        <a href="#">Help/FAQ</a></li>
+        <li<?php if ($thisPage=="07About")
+        echo " id=\"currentpage\""; ?>>
+        <a href="#">About Us</a></li>
+        <li<?php if ($thisPage=="01LogonPage")
+        echo " id=\"currentpage\""; ?>>
+        <a href="#">Logon</a></li>
+        <li<?php if ($thisPage=="02UserHome")
+        echo " id=\"currentpage\""; ?>>
+        <a href="#">My Students</a></li>
+      </ul>
+    </div>
 </html>
