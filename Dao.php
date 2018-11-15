ef23771aa02db08a355a3b6e0f341f7f3a1b9437
ryanskeesuck <ryanskeesuck@u.boisestate.edu>
2018-11-14 17:23:32 -0700

    again

--- Dao.php
+++ Dao.php
@@ -20,11 +20,13 @@ class Dao{
 
 public function addUser($username, $password){
       //$this->log->LogInfo("Save comment [{$name}][{$comment}]");
+      $salt=$password . $username;
+      $hashPass = hash('sha256', $salt);
       $conn=$this->getConnection();
 			$saveQuery = $conn->prepare(
 				"INSERT INTO accounts (username, password) VALUES (:username, :password)");
 			$saveQuery->bindParam(":username", $username);
-			$saveQuery->bindParam(":password", $password);
+			$saveQuery->bindParam(":password", $hashPass);
       $saveQuery->execute();
   }
 
@@ -37,10 +39,12 @@ public function addUser($username, $password){
 }
 
   public function getUserPassword($username, $password){
-		$conn=$this->getConnection();
+    $salt=$password . $username;
+    $hashPass = hash('sha256', $salt);
+    $conn=$this->getConnection();
 		$q=$conn->prepare("SELECT username FROM accounts WHERE username=:username AND password=:password");
 		$q->bindParam(":username", $username);
-		$q->bindParam(":passwords", $password);
+		$q->bindParam(":password", $hashPass);
 		$q->setFetchMode(PDO::FETCH_ASSOC);
 		$q->execute();
 		$result=$q->fetchAll();
@@ -48,6 +52,8 @@ public function addUser($username, $password){
 	}
 
     public function validateUser($username, $password){
+      $salt=$password . $username;
+      $hashPass = hash('sha256', $salt);
       $conn=$this->getConnection();
       $stmt = $conn->prepare("SELECT * FROM accounts WHERE username = :username");
       $stmt->bindparam(":username", $username);
@@ -55,7 +61,7 @@ public function addUser($username, $password){
       $user = $stmt->fetch();
       if ($user){
         $digest = $user['password'];
-          if($password == $digest){
+          if($hashPass == $digest){
             return true;
           }
           return false;
