788881ef04a36c4cba3f075d9f65875e19488153
ryanskeesuck <ryanskeesuck@u.boisestate.edu>
2018-11-14 17:43:28 -0700

    again

--- head.php
+++ head.php
@@ -1,3 +1,4 @@
 <link rel = "stylesheet" href="stylesheet.css"/>
   <title>SchoolBROS:Main</title>
   <link rel="icon" href="FavICON.png" type="image/gif" sizes="16x16">
+  <script src="jquery-3.3.1.min.js"></script>
