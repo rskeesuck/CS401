c1c5c08303eb66e5bc9816f99826ec5cd97ef567
ryanskeesuck <ryanskeesuck@u.boisestate.edu>
2018-11-14 20:23:42 -0700

    Try, try again

--- head.php
+++ head.php
@@ -2,3 +2,4 @@
   <title>SchoolBROS</title>
   <link rel="icon" href="FavICON.png" type="image/gif" sizes="16x16">
   <script src="jquery-3.3.1.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Advent+Pro" rel="stylesheet">
